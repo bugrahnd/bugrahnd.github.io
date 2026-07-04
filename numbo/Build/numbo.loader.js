f3b697a6df803dacefd49c6820764d11653f7677
Bugrahnd <bghrndvc@gmail.com>
2026-07-04 22:16:57 +0300

    sonuc

--- numbo/Build/numbo.loader.js
+++ numbo/Build/numbo.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 57884641,
+    wasmFileSize: 57887519,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
