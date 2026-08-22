6383ae76630cd98a211fab175a27a7da19f907ce
Bugrahnd <bghrndvc@gmail.com>
2026-08-23 00:04:14 +0300

    kısmi ui

--- tileup/Build/tileup.loader.js
+++ tileup/Build/tileup.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58939262,
+    wasmFileSize: 58943199,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
